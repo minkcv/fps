828a1a2b0b9be980ed74fb40fd1a1bbdb1134e94
Will Smith <minkcv@gmail.com>
2017-12-21 16:12:24 -0700

    add last section and space art

--- main.js
+++ main.js
@@ -138,11 +138,11 @@ function init() {
     //loadLevel1();
     //TH.clearScene();
     //MA.clearWorld();
-    //loadLevel2();
     loadLevel2();
     loadLevel3();
     loadLevel4();
     loadLevel5();
+    loadLevel6();
 }
 init();
 
